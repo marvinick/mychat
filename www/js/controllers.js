f16ed2b43c13b5179fe99d14c1c877f3cb278ec6
Marvki <marvkipi@gmail.com>
2016-04-28 22:33:29 -0700

    controllers setup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,30 @@
 angular.module('mychat.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('LoginCtrl', function($scope, $ionicModal, $state) {
+  console.log('Login Controller Initialized');
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  $ionicModal.fromTemplateUrl('templates/signup.html', {
+    scope: $scope
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
 
+  $scope.createUser = function(user) {
+
+  }
+
+  scope.signIn = function() {
+    $state.go('tab.rooms');
+  }
+})
+
+.controller('ChatCtrl', function($scope, Chats) {
+  console.log("Chat Controller initialized");
   $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }; 
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('RoomsCtrl', function($scope, Rooms) {
+  console.log("Rooms Controller Initialized")
+  $scope.rooms = Rooms.all();
 });
+
